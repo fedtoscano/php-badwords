8403b766c642eea9953473a4151a654a003a7dcc
fedtoscano <federicotoscano611@gmail.com>
2024-06-25 17:21:01 +0200

    aggiunta istruzione condizionale col metodo is set per non renderizzare l-errore qualora la variabile wordToFilter non sia stata definita

--- index.php
+++ index.php
@@ -1,8 +1,14 @@
 <?php 
     $paragrafo = "Mio caro Marco,
 Sono andato stamattina dal mio medico, Ermogene, recentemente rientrato in Villa da un lungo viaggio in Asia. Bisognava che mi visitasse a digiuno ed eravamo d'accordo per incontrarci di primo mattino. Ho deposto mantello e tunica; mi sono adagiato sul letto. Ti risparmio particolari che sarebbero altrettanto sgradevoli per te quanto lo sono per me, e la descrizione del corpo d'un uomo che s'inoltra negli anni ed è vicino a morire di un'idropisia del cuore. Diciamo solo che ho tossito, respirato, trattenuto il fiato, secondo le indicazioni di Ermogene, allarmato suo malgrado per la rapidità dei progressi del male, pronto ad attribuirne la colpa al giovane Giolla, che m’ha curato in sua assenza. È difficile rimanere imperatore in presenza di un medico; difficile anche conservare la propria essenza umana: l’occhio del medico non vede in me che un aggregato di umori, povero amalgama di linfa e di sangue. E per la prima volta, stamane, m’è venuto in mente che il mio corpo, compagno fedele, amico sicuro e a me noto più dell’anima, è solo un mostro subdolo che finirà per divorare il padrone. Basta… Il mio corpo mi è caro; mi ha servito bene, e in tutti i modi, e non starò a lesinargli le cure necessarie. Ma, ormai, non credo più, come finge ancora Ermogene, nelle virtù prodigiose delle piante, nella dosatura precisa di quei sali minerali che è andato a procurarsi in Oriente. È un uomo fine; eppure, m’ha propinato formule vaghe di conforto, troppo ovvie per poterci credere; sa bene quanto detesto questo genere d’imposture, ma non si esercita impunemente più di trent’anni la medicina. Perdono a questo mio fedele il suo tentativo di nascondermi la mia morte. Ermogene è dotto; è persino saggio; la sua probità è di gran lunga superiore a quella d’un qualunque medico di corte. Avrò in sorte d’essere il più curato dei malati. Ma nessuno può oltrepassare i limiti prescritti dalla natura; le gambe gonfie non mi sostengono più nelle lunghe cerimonie di Roma; mi sento soffocare; e ho sessant’anni.";
-    $wordToFilter = $_GET["wordToFilter"];
-    $fitleredStr = str_replace("$wordToFilter", "***", "$paragrafo")
+    
+    if(isset($_GET["wordToFilter"])){
+        $wordToFilter = $_GET["wordToFilter"];
+        $fitleredStr = str_replace("$wordToFilter", "***", "$paragrafo");    
+    }else{
+        $wordToFilter = '';
+        $fitleredStr = $paragrafo;
+    }
 ?>
 
 <!DOCTYPE html>
